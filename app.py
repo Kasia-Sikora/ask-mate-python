42124b9ef47c5a445efd78904426153844ee1019
bkwoka <bkwoka1@gmail.com>
2019-09-03 14:45:45 +0200

    add content to index.html

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     user_questions = connection.get_all_questions()
-    user_answers = connection.get_all_answers()
-    print(user_questions)
-    print(user_answers)
-    return render_template('index.html')
+    return render_template('list.html', uesr_questions=user_questions)
 
 
 if __name__ == '__main__':
