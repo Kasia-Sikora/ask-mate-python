4d95c4b40540099d3d2b5f126941a37398d5e0d2
bkwoka <bkwoka1@gmail.com>
2019-09-03 23:37:08 +0200

    New design, and couple frontend file cd...

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     user_questions = connection.get_all_questions()
+    print(user_questions)
     return render_template('list.html', uesr_questions=user_questions)
 
 
