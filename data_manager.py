2340d63d3584032fa604abe915582b1da2b46f05
ruda <sikora.katarzyna.waw@gmail.com>
2019-09-04 14:16:48 +0200

    Add functions to change vote numbers Add functions to change view numbers

--- data_manager.py
+++ data_manager.py
@@ -3,10 +3,13 @@ import connection
 
 def search_for_question(question_id):
     user_questions = connection.get_all_questions()
-    for question in user_questions:
+    for i, question in enumerate(user_questions):
         if str(question_id) == question["id"]:
             question = change_view_number(question)
-            return question
+            user_questions[i] = question
+            break
+    connection.update_questions(user_questions)
+    return question
 
 
 def search_for_all_answers(answer_id):
@@ -22,3 +25,25 @@ def change_view_number(question):
     view = int(question['view_number']) + 1
     question['view_number'] = view
     return question
+
+
+def change_question_vote_number(id, vote_value):
+    user_questions = connection.get_all_questions()
+    for question in user_questions:
+        if str(id) == question['id']:
+            vote = int(question['vote_number']) + vote_value
+            question['vote_number'] = vote
+            break
+    connection.update_questions(user_questions)
+    return question
+
+
+def change_answer_vote_number(id, vote_value):
+    user_answers = connection.get_all_answers()
+    for answer in user_answers:
+        if str(id) == answer['id']:
+            vote = int(answer['vote_number']) + vote_value
+            answer['vote_number'] = vote
+            break
+    connection.update_answers(user_answers)
+    return answer
