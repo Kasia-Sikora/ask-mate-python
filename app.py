8a7e31252baf1188d9284cd8ca6d8d6bc4adfdc6
bkwoka <bkwoka1@gmail.com>
2019-10-02 14:27:21 +0200

    refactor session

--- app.py
+++ app.py
@@ -3,198 +3,178 @@ import data_manager
 import os
 import util
 
+
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 
 @app.route('/')
 def home():
-    user = util.check_session_usr()
     user_questions = data_manager.get_all_questions()
-    return render_template('list.html', user_questions=user_questions, user=user)
+    return render_template('list.html', user_questions=user_questions)
 
 
 @app.route('/add-question')
 def add_question():
-    user = util.check_session_usr()
-    return render_template('new_quest_form.html', user=user)
+    return render_template('new_quest_form.html')
 
 
 @app.route('/question-form', methods=['GET', 'POST'])
 def question_form():
-    user = util.check_session_usr()
     if request.method == 'POST':
         dict_new_quest = dict(request.form)
         quest_details = data_manager.save_question(dict_new_quest)
         return render_template('question_details.html',
-                               question_details=quest_details,
-                               user=user)
+                               question_details=quest_details)
 
 
 @app.route('/question/<question_id>')
 def question_details(question_id):
-    user = util.check_session_usr()
     quest = data_manager.search_for_question(question_id)
     answers = data_manager.search_for_all_answers(question_id)
     return render_template('question_details.html',
                            question_details=quest,
-                           answers=answers,
-                           user=user)
+                           answers=answers)
 
 
 @app.route('/question/<question_id>/new_comment')
 def new_comment(question_id):
-    user = util.check_session_usr()
-    return render_template('new_comment_form.html', question_id=question_id, user=user)
+    
+    return render_template('new_comment_form.html', question_id=question_id)
 
 
 @app.route('/comment_form', methods=['GET', 'POST'])
 def comment_form():
-    user = util.check_session_usr()
+    
     if request.method == 'POST':
         dict_comment = request.form
         data_manager.save_question_comment(dict_comment)
-        return redirect(url_for('comment_details', question_id=dict_comment['question_id'], user=user))
+        return redirect(url_for('comment_details', question_id=dict_comment['question_id']))
 
 
 @app.route('/question/<question_id>/comments')
 def comment_details(question_id):
-    user = util.check_session_usr()
     quest = data_manager.search_for_question(question_id)
     comments = data_manager.get_all_question_comments(question_id)
     return render_template('comment_details.html',
-                           question_details=quest, comments=comments, user=user)
+                           question_details=quest, comments=comments)
 
 
 @app.route('/question/<question_id>/new-answer')
 def add_answer(question_id):
-    user = util.check_session_usr()
-    return render_template('new_answer_form.html', question_id=question_id, user=user)
+    return render_template('new_answer_form.html', question_id=question_id)
 
 
 @app.route('/answer_form', methods=['GET', 'POST'])
 def answer_form():
-    user = util.check_session_usr()
     if request.method == 'POST':
         dict_new_answer = request.form
         data_manager.new_answer(dict_new_answer)
-        return redirect(url_for('question_details', question_id=dict_new_answer['question_id'], user=user))
+        return redirect(url_for('question_details', question_id=dict_new_answer['question_id']))
 
 
 @app.route('/answer/<answer_id>/new-comment')
 def new_answer_comment(answer_id):
-    user = util.check_session_usr()
-    return render_template('answer_comment_form.html', answer_id=answer_id, user=user)
+    
+    return render_template('answer_comment_form.html', answer_id=answer_id)
 
 
 @app.route('/answer_comment_form', methods=['GET', 'POST'])
 def answer_comment_form():
-    user = util.check_session_usr()
     if request.method == 'POST':
         dict_comment = request.form
         data_manager.save_answer_comment(dict_comment)
-        return redirect(url_for('answer_comment_details', answer_id=dict_comment['answer_id'], user=user))
+        return redirect(url_for('answer_comment_details', answer_id=dict_comment['answer_id']))
 
 
 @app.route('/answer/<answer_id>/d')
 def answer_comment_details(answer_id):
-    user = util.check_session_usr()
     answer = data_manager.search_for_answer(answer_id)
     comments = data_manager.get_all_answer_comments(answer_id)
     return render_template('comment_details.html',
-                           answer_details=answer, comments=comments, user=user)
+                           answer_details=answer, comments=comments)
 
 
 @app.route('/question/<question_id>/answer-up')
 def answer_vote_up(question_id):
-    user = util.check_session_usr()
     info_dict = {
         'id_answer': request.args.get('answer_id'),
         "vote": 1
     }
     data_manager.change_answer_vote(dictionary=info_dict)
-    return redirect(url_for('question_details', question_id=question_id, user=user))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/question/<question_id>/answer-down')
 def answer_vote_down(question_id):
-    user = util.check_session_usr()
     info_dict = {
         'id_answer': request.args.get('answer_id'),
         "vote": -1
     }
     data_manager.change_answer_vote(dictionary=info_dict)
-    return redirect(url_for('question_details', question_id=question_id, user=user))
+    return redirect(url_for('question_details', question_id=question_id))
 
 
 @app.route('/question/<question_id>/vote-up')
 def question_vote_up(question_id):
-    user = util.check_session_usr()
     info_dict = {
         "vote": 1,
         "question_id": question_id
     }
     data_manager.change_question_vote(dictionary=info_dict)
-    return redirect(url_for('home', user=user))
+    return redirect(url_for('home'))
 
 
 @app.route('/question/<question_id>/vote-down')
 def question_vote_down(question_id):
-    user = util.check_session_usr()
     info_dict = {
 
         "vote": -1,
         "question_id": question_id
     }
     data_manager.change_question_vote(dictionary=info_dict)
-    return redirect(url_for('home', user=user))
+    return redirect(url_for('home'))
 
 
 @app.route('/answer/<answer_id>/edit')
 def edit_answer(answer_id):
-    user = util.check_session_usr()
     answer_detail = data_manager.get_answer(answer_id)
-    return render_template('edit_answer_form.html', answer_detail=answer_detail, user=user)
+    return render_template('edit_answer_form.html', answer_detail=answer_detail)
 
 
 @app.route('/update_answer/<answer_id>', methods=['GET', 'POST'])
 def update_answer(answer_id):
-    user = util.check_session_usr()
     if request.method == 'POST':
         form_dict = dict(request.form)
         form_dict['answer_id'] = answer_id
         dict_from_dm = data_manager.update_answer(answer_dict=form_dict)
-        return redirect(url_for('question_details', question_id=dict_from_dm['question_id'], user=user))
+        return redirect(url_for('question_details', question_id=dict_from_dm['question_id']))
 
 
 @app.route('/comment/<comment_id>/edit')
 def edit_comment_form(comment_id):
-    user = util.check_session_usr()
     comment_det = data_manager.get_comment(comment_id=comment_id)
-    return render_template('edit_comment_form.html', comment_det=comment_det, user=user)
+    return render_template('edit_comment_form.html', comment_det=comment_det)
 
 
 @app.route('/comment/<comment_id>/form', methods=['GET', 'POST'])
 def form_comment(comment_id):
-    user = util.check_session_usr()
     form_dict = dict(request.form)
     form_dict['comment_id'] = comment_id
     comment_det = data_manager.update_comment(form_dict=form_dict)
     if comment_det['question_id'] is not None:
-        return redirect(url_for('question_details', question_id=comment_det['question_id'], user=user))
+        return redirect(url_for('question_details', question_id=comment_det['question_id']))
 
 
 @app.route('/comment/<comment_id>/delete')
 def delete_comment(comment_id):
-    user = util.check_session_usr()
     answ_id = data_manager.delete_comment(comment_id)
     answer_id = answ_id['answer_id']
-    return redirect(url_for('answer_comment_details', answer_id=answer_id, user=user))
+    return redirect(url_for('answer_comment_details', answer_id=answer_id))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    user = util.check_session_usr()
     if request.method == 'POST':
         user_data = request.form.to_dict()
         check_login = data_manager.check_login(user_data)
@@ -204,7 +184,7 @@ def login():
         verify = util.verify_password(user_data['password'], check_login['password'])
         if verify:
             session['username'] = request.form['login']
-            return redirect(url_for('home', user=user))
+            return redirect(url_for('home'))
         else:
             message = 'Invalid login or password'
             return render_template('login.html', message=message)
@@ -213,13 +193,11 @@ def login():
 
 @app.route('/registration')
 def registration():
-    user = util.check_session_usr()
-    return render_template('registration.html', user=user)
+    return render_template('registration.html')
 
 
 @app.route('/registration-form', methods=['GET', 'POST'])
 def registration_form():
-    user = util.check_session_usr()
     if request.method == 'POST':
         registration_dict = request.form.to_dict()
         hashed_pass = util.hash_password(registration_dict['password'])
@@ -227,14 +205,13 @@ def registration_form():
         saved_login = data_manager.new_user(registration_dict)
         if saved_login == registration_dict['username']:
             session['username'] = request.form['username']
-            return redirect(url_for('login', user=user))
+            return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
-    user = util.check_session_usr()
     session.pop('username', None)
-    return redirect(url_for('home', user=user))
+    return redirect(url_for('home'))
 
 
 @app.route('/users-list')
@@ -254,7 +231,6 @@ def user_details(username):
     #         raise KeyError
     # except KeyError:
     #     return redirect(url_for('login'))
-
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -10,11 +10,3 @@ def hash_password(plain_text_password):
 def verify_password(plain_text_password, hashed_password):
     hashed_bytes_password = hashed_password.encode('utf-8')
     return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
-
-
-def check_session_usr():
-    try:
-        return session['username']
-    except KeyError:
-        return None
-
